9f5e4bcb4d32c1f271f2ab1b8ff225966d2d0107
0d-fako <od.fakorede@gmail.com>
2025-07-21 17:24:55 +0100

    Popup.js impl

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadBtn = document.getElementById("downloadBtn");
+    const statusText = document.getElementById("statusText");
+    const info = document.getElementById("info");
+
+
+    downloadBtn
+
+
+})
